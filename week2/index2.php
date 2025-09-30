42608c47667a26f90a14339bc7a8204a7e9a7747
Daduei <phungducduyip@gmail.com>
2025-09-30 22:10:44 +0700

    update

---
+++ week2/index2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html!>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+       <h1>My first PHP page</h1>
+       <p>
+            <?php
+            $x = 8;
+            echo "The value of x is: " . $x;
+            ?>
+    </body>
+</html>
